53bbd3ffb6f75fb28d64ede979b3973488cf266d
Martin Förster <martinfoerster95@gmx.de>
2020-12-30 12:13:04 +0100

    gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "eu.ownii"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
